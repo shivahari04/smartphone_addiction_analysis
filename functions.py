f807550a1bd18ced608e1cfdd9f4ef15e50d1de4
shivahari04 <shivahari04@yahoo.com>
2026-07-27 08:52:17 -0400

    Final steps of Data cleaning and building new CSVs

--- functions.py
+++ functions.py
@@ -1,8 +1,7 @@
-# Function 1: Check duplicates in both datasets and removing them if there is any.
-
-
 import pandas as pd
 
+
+# Function 1: Check duplicates in both datasets and removing them if there is any.
 def check_duplicates (df, name ="dataset"):
     dup_count = df.duplicated().sum()
     print(f"{name} has duplicate rows: {dup_count}")
@@ -10,7 +9,7 @@ def check_duplicates (df, name ="dataset"):
     
 
 
-# Function 2 : Changing Anxiety_Level and Depression_Level values - Scaling down to 1-10 range from 1-100
+# Function 2 : For global_smartphone_addiction dataset - Changing Anxiety_Level and Depression_Level values - Scaling down to 1-10 range from 1-100
 def scale_to_ten (df, column_name, threshold=10):
     """
     Scales a column down to a 0-10 range by dividing by 10,
